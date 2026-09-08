3695bd9322a097000b577dd3d43d6f1a69c9147d
avaneeko <222704844+avaneeko@users.noreply.github.com>
2026-09-08 13:39:03 +0300

    add: Two-stage MariaDB container Dockerfile configuaration fix: MariaDB build script. fix: Replace dead MariaDB source tarball URL with latest working mirror.

---
+++ srcs/requirements/mariadb/tools/InstallMariaDBRuntimeDependencies.sh
@@ -0,0 +1,5 @@
+###############################################
+# Runtime dependencies installer for MariaDB.
+###############################################
+
+apk add ncurses gnutls bzip2 lz4 xz-libs lzo curl snappy boost libaio
